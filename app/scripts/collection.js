3d9f04359989d4e8b1e1e50761efde24463c98bc
bholloway546 <bholloway546@gmail.com>
2014-12-31 19:27:14 +0000

    Overlays with jQuery v2

--- app/scripts/collection.js
+++ app/scripts/collection.js
@@ -2,9 +2,9 @@
     var template =
         '<div class="collection-album-container col-md-2">'
       + '  <div class="collection-album-image-container">'
-   + '    <img src="/images/album-placeholder.png"/>'
-   + '  </div>'
-      + '  <div class="caption album-collection-info">'
+      + '    <img src="/images/album-placeholder.png"/>'
+      + '  </div>'
+      + '    <div class="caption album-collection-info">'
       + '    <p>'
       + '      <a class="album-name" href="/album.html"> Album Name </a>'
       + '      <br/>'
@@ -17,24 +17,45 @@
       + '    </p>'
       + '  </div>'
       + '</div>';
- 
-   return $(template);
+    return $(template);
  };
- 
+   
+var buildAlbumOverlay = function(albumURL) {
+    var template =
+        '<div class="collection-album-image-overlay">'
+      + '  <div class="collection-overlay-content">'
+      + '    <a class="collection-overlay-button" href="' + albumURL + '">'
+      + '      <i class="fa fa-play"></i>'
+      + '    </a>'
+      + '    &nbsp;'
+      + '    <a class="collection-overlay-button">'
+      + '      <i class="fa fa-plus"></i>'
+      + '    </a>'
+      + '  </div>'
+      + '</div>'
+      ;
+    return $(template);
+  };
+  
 var updateCollectionView = function() {
    var $collection = $(".collection-container .row");
    $collection.empty();
- 
+
    for (var i = 0; i < Math.floor((Math.random() * 100) + 1); i++) {
      var $newThumbnail = buildAlbumThumbnail();
      $collection.append($newThumbnail);
    }
+ 
+var onHover = function(event) {
+     $(this).append(buildAlbumOverlay("/album.html"));
+   };
+   
    var offHover = function(event) {
     $(this).find('.collection-album-image-overlay').remove();
   };
-
+   
   $collection.find('.collection-album-image-container').hover(onHover, offHover);
- };
+}
  
 if (document.URL.match(/\/collection.html/)) {
   // Wait until the HTML is fully processed.
