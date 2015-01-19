89738ab97f0c360537724e65c9389e51d4804ab8
bholloway546 <bholloway546@gmail.com>
2015-01-19 05:19:39 +0000

    Functional Player Controls

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -181,6 +181,7 @@ var createSongRow = function(songNumber, songName, songLength) {
 };
 
  var changeAlbumView = function(album) {
+   
    // Update the album title
    var $albumTitle = $('.album-title');
    $albumTitle.text(album.name);
@@ -196,17 +197,59 @@ var createSongRow = function(songNumber, songName, songLength) {
    // Update the album image
    var $albumImage = $('.album-image img');
    $albumImage.attr('src', album.albumArtUrl);
- 
+   
    // Update the Song List
    var $songList = $(".album-song-listing");
    $songList.empty();
    var songs = album.songs;
+   
    for (var i = 0; i < songs.length; i++) {
      var songData = songs[i];
      var $newRow = createSongRow(i + 1, songData.name, songData.length);
      $songList.append($newRow);
+  
    }
  
+ };
+
+var updateSeekPercentage = function($seekBar, event) {
+   var barWidth = $seekBar.width();
+   var offsetX = event.pageX - $seekBar.offset().left;
+ 
+   var offsetXPercent = (offsetX  / barWidth) * 100;
+   offsetXPercent = Math.max(0, offsetXPercent);
+   offsetXPercent = Math.min(100, offsetXPercent);
+ 
+   var percentageString = offsetXPercent + '%';
+   $seekBar.find('.fill').width(percentageString);
+   $seekBar.find('.thumb').css({left: percentageString});
+ }
+
+var setupSeekBars = function() {
+ 
+   $seekBars = $('.player-bar .seek-bar');
+   $seekBars.click(function(event) {
+     updateSeekPercentage($(this), event);
+   });
+ 
+   $seekBars.find('.thumb').mousedown(function(event){
+    var $seekBar = $(this).parent();
+     
+     $seekBar.addClass('no-animate');
+ 
+    $(document).bind('mousemove.thumb', function(event){
+      updateSeekPercentage($seekBar, event);
+    });
+ 
+    //cleanup
+    $(document).bind('mouseup.thumb', function(){
+      $seekBar.removeClass('no-animate');
+      
+      $(document).unbind('mousemove.thumb');
+      $(document).unbind('mouseup.thumb');
+    });
+ 
+  });
  };
 
 // This 'if' condition is used to prevent the jQuery modifications
@@ -215,7 +258,8 @@ var createSongRow = function(songNumber, songName, songLength) {
  if (document.URL.match(/\/album.html/)) {
    // Wait until the HTML is fully processed.
    $(document).ready(function() {
-   changeAlbumView(albumPicasso);
+   changeAlbumView(albumPicasso)
+     setupSeekBars();
    });
  }
 });
